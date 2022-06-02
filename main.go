553c1bfa77f5ae87c63a0f78b9a5c57f09ffd724
Anirudh Rao <anirudh131299@gmail.com>
2022-06-02 23:40:59 +0530

    File Structure Created

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type Block struct{
+	Pos int
+	Data BookCheckout
+	Timestamp string
+	Hash string
+	PrevHash string
+}
+
+type BookCheckout struct {
+	BookID       string `json:"book_id"`
+	User         string `json:"user"`
+	CheckoutDate string `json:"checkout_date"`
+	IsGenesis    bool   `json:"is_genesis"`
+}
+
+type Book struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	PublishDate string `json:"publish_date"`
+	ISBN        string `json:"isbn:`
+}
+
+//To Generate hash 
+func (b *Block) generateHash() {
+	// get string val of the Data
+	bytes, _ := json.Marshal(b.Data)
+	// concatenate the dataset
+	data := string(b.Pos) + b.Timestamp + string(bytes) + b.PrevHash
+	hash := sha256.New()
+	hash.Write([]byte(data))
+	b.Hash = hex.EncodeToString(hash.Sum(nil))
+}
+
